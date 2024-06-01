1ddd8b4dfc666e0edb2b33ad9bc9e31e609265cc
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-06-01 16:06:57 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- SAIFALLBOT/saifban.py
+++ SAIFALLBOT/saifban.py
@@ -1,1 +1,52 @@
+#  Copyright (C) 2024-25 @SAIF_DICTATOR - DICTATOR
+# TELEGRAM BAN ALL BOT
+# CREATOR - SAIF PAPA
 
+
+from telethon import events
+# Don't mess with this dict code if u don't know about this !!
+ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
+dady = (ex['f']+ex['a']+ex['s']+ex['s']+ex['z']+ex['o']+ex['c']+ex['c']+ex['d']+ex['k'])
+dad = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
+
+SAISTART = """ʜᴇʟʟᴏ sɪʀ [{}](tg://user?id={}) ɪ ᴀᴍ sᴀɪғʜʙᴀɴᴀʟʟ ʙᴏᴛ,
+
+__ɪ ᴀᴍ ᴀʟɪᴠᴇ ᴀʟʀᴇᴀᴅʏ ᴍᴀsᴛᴇʀ !
+ᴊᴜsᴛ ʀᴇᴀᴅʏ ᴛᴏ ᴋɪʟʟ ᴀɴʏ ɢʀᴏᴜᴘ
+ᴀᴅᴅ ᴍᴇ ᴛʜᴇʀᴇ ᴀɴᴅ ɪ ᴡɪʟʟ  ᴋɪʟʟ ɢʀᴏᴜᴘ !__
+
+**sᴀɪғ-ʙᴀɴᴀʟʟ ʙᴏᴛ  ɪɴғᴏ!**
+➥ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ » `{}`
+➥ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ » `{}`
+➥ ᴄᴏᴅᴇ ᴏᴡɴᴇʀ » [ᴅɪᴄᴛᴀᴛᴏʀ](https://t.me/SAIF_DICTATOR)
+➥ ᴊᴏɪɴ ɢʀᴏᴜᴘ » [ sᴀɪғʙᴏᴛᴢ](https://t.me/SAIFALLBOT)
+
+ᴛʏᴘᴇ /help ғᴏʀ ᴄᴏᴍᴍᴀɴᴅs!
+"""
+
+
+SSTART = """ʜᴇʟʟᴏ ʙᴀʙʏ [{}](tg://user?id={}),
+
+__ᴛʜɪs ɪs ʙᴀɴᴀʟʟ ʙᴏᴛ . ᴍʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴏɴʟʏ ᴀᴄᴄᴇss ᴛʜɪs ʙᴏᴛ !__
+
+**sᴀɪғ-ʙᴀɴᴀʟʟ ʙᴏᴛ  ɪɴғᴏ!**
+➥ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ » `{}`
+➥ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ » `{}`
+➥ ᴄᴏᴅᴇ ᴏᴡɴᴇʀ » [ᴅɪᴄᴛᴀᴛᴏʀ](https://t.me/SAIF_DICTATOR)
+➥ ᴊᴏɪɴ ɢʀᴏᴜᴘ » [ sᴀɪғʙᴏᴛᴢ](https://t.me/SAIFALLBOT)
+"""
+
+SAIHELP = """ʜᴇʟʟᴏ ᴅᴇᴀʀ [{}](tg://user?id={}) ᴛʜɪs ɪs sᴀɪғ ʙᴀɴ ᴀʟʟ ʙᴏᴛ,
+
+__ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ__:
+
+➣ /start - ᴛᴏ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ.
+➣ /ping - ᴄʜᴇᴄᴋ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ.
+➣ /banall - ᴛᴏ ʙᴀɴᴀʟʟ  ɪɴ ɢʀᴏᴜᴘ.
+➣ /leave - ᴛᴏ ʟᴇᴀᴠᴇ ɢʀᴏᴜᴘ.
+➣ /unbanall - ᴛᴏ ᴜɴʙᴀɴᴀʟʟ ɪɴ ɢʀᴘ
+➣ /kickall - ᴛᴏ ᴋɪᴄᴋ ᴀʟʟ ᴜsᴇʀ ғʀᴏᴍ ɢʀᴘ
+➣ /restart - ᴛᴏ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ.
+
+➥ ғᴀᴄɪɴɢ ᴀɴʏ ᴋɪɴᴅ ᴏғ ɪssᴜᴇ ᴛʜᴇɴ ᴊᴜsᴛ ᴊᴏɪɴ ᴏᴜʀ ||[sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ](https://t.me/SAIFALLBOT)|| ᴀɴᴅ ᴀsᴋ ᴛʜᴇʀᴇ!!
+""" 
