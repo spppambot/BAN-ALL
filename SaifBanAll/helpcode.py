1287f716d97f93ad633f77f11a3cd59430b90940
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-06-01 16:58:27 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

---
+++ SaifBanAll/helpcode.py
@@ -0,0 +1,5 @@
+from telethon import events
+# Don't mess with this dict code if u don't know about this !!
+ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
+dady = (ex['f']+ex['a']+ex['s']+ex['s']+ex['z']+ex['o']+ex['c']+ex['c']+ex['d']+ex['k'])
+dad = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
